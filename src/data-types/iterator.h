b46616e237d261f18f6f93c87e0fb31a5a6155bb
Steve Shea <stevenlsjr@gmail.com>
2015-06-23 17:30:16 -0500

    added iterator type for facilitating looping through collections

---
+++ src/data-types/iterator.h
@@ -0,0 +1,11 @@
+//
+// Created by Steven on 6/23/15.
+//
+
+#ifndef DANGERENGINE_ITERATOR_H
+#define DANGERENGINE_ITERATOR_H
+
+typedef slsIterator slsIterator;
+
+
+#endif //DANGERENGINE_ITERATOR_H
