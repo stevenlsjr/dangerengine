7ca73016f7b755c43030bc58e5ae0333edcd1bf9
Steve Shea <stevenlsjr@gmail.com>
2015-07-12 09:38:31 -0500

    started removing function pointer methods for true function methods

--- src/data-types/array.h
+++ src/data-types/array.h
@@ -66,6 +66,36 @@ slsArray *sls_array_new(void const *array,
                         size_t n_elements);
 
 
+size_t sls_array_length(slsArray const *self);
+
+size_t sls_array_element_size(slsArray const *self);
+
+void const *sls_array_get(slsArray const *self, size_t i);
+
+/**
+ * @brief sets the value of a current element
+ * @details [long description]
+ *
+ * @param t [description]
+ * @param i [description]
+ * @param value [description]
+ * @return [description]
+ */
+void sls_array_set(slsArray *self, size_t i, void *value);
+
+/**
+ * @brief inserts an element, pushing back elements ahead in the array.
+ * @details [long description]
+ *
+ * @param t [description]
+ * @param i [description]
+ * @param value [description]
+ * @return [description]
+ */
+void sls_array_insert(slsArray *self, size_t i, void *value);
+
+slsArray *sls_array_copy(slsArray const *self);
+
 /**
  * @brief convenience method for retrieving index values.
  * @detail WARNING: dereferences pointer given by slsArray::get
