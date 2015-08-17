a8746256918c59d98756ccbadd0fb37d59e04365
Steve Shea <stevenlsjr@gmail.com>
2015-08-17 09:59:54 -0500

    worked on sprite renderer and sprite types

--- src/renderer/slsSpriteRenderer.h
+++ src/renderer/slsSpriteRenderer.h
@@ -11,20 +11,48 @@
 #include "slstexture.h"
 
 #include <data-types/ptrarray.h>
+#include "../math/slsTransform2D.h"
 
 typedef struct slsSprite slsSprite;
 typedef struct slsSpriteBatch slsSpriteBatch;
 
 struct slsSprite {
+
+  slsSprite *(*init)(slsSprite *self,
+                     slsTexture *tex,
+                     float const uvs[8],
+                     slsTransform2D const *transform);
+
+  slsSprite *(*dtor)(slsSprite *self);
+
+  void (*draw)(slsSprite *self, slsMesh *mesh);
+
+  slsTransform2D const *(*get_transform)(slsSprite *self);
+
+  void (*set_transform)(slsSprite *self, slsTransform2D const *transform);
+
   slsTexture *texture;
+  float sprite_uvs[8];
+
+
+  slsTransform2D transform2D;
+  kmMat4 transform;
+
+  void *data;
 };
 
 struct slsSpriteBatch {
   slsPtrArray *sprites;
 };
 
-slsSpriteBatch *sls_spritebatch_init(slsSpriteBatch *self);
 
+slsSprite *sls_sprite_new(slsTexture *tex,
+                          float const sprite_uvs[8],
+                          slsTransform2D const *transform);
+
+slsSprite const *sls_sprite_class();
+
+slsSpriteBatch *sls_spritebatch_init(slsSpriteBatch *self);
 slsSpriteBatch *sls_spritebatch_dtor(slsSpriteBatch *self);
 
 #endif //DANGERENGINE_SLSSPRITERENDERER_H
